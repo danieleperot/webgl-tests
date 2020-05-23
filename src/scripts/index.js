2a5597b9e06f5dd5149b7b6bbbf683824e980fc3
Daniele Perot <perotdaniele@gmail.com>
2020-05-23 12:44:24 +0300

    first page of the tutorial with javascript

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,22 @@
 import '../styles/index.scss'
 
-console.log('webpack starterkit')
+/**
+ * My main function
+ */
+function main() {
+  const canvas = document.querySelector('#glCanvas')
+  const gl = canvas.getContext('webgl')
+
+  if (gl === null) {
+    console.warn('WebGL is not supported.')
+    return
+  }
+
+  // Set clear color to black
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+  // Clear the color buffer with the specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT)
+}
+
+window.onload = main
